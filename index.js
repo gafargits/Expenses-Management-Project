c141718201f737b39067ce81f8ce1cd650c09358
gafargits <rajigafaronline@gmail.com>
2019-12-17 19:41:17 +0100

    clients path corrected

--- index.js
+++ index.js
@@ -34,9 +34,9 @@ app.use(bodyParser.json())
 app.use(morgan('combined'))
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, './client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
   app.use((req, res)=>{
-    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+    res.sendFile(path.join(__dirname,'../client/build/index.html'))
   })
 }
 
